f585420d470cd9ae98d383cbc0d8ace448cfa00b
voleger <dev.vegera@gmail.com>
2022-09-14 23:58:47 +0300

    Fix typo in global script

--- docs/js/global.js
+++ docs/js/global.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const backlink = document.querySelector('[data-backlink]')
     if (backlink) {
         if (document.referrer === '') {
-            backlink.styles.display = 'none';
+            backlink.style.display = 'none';
         }
         else {
             backlink.addEventListener('click', historyBack);

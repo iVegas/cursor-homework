8dba7a6c2306191effae792461d1901730954473
voleger <dev.vegera@gmail.com>
2022-09-27 09:23:20 +0300

    fix handling error on sass pipeline

--- source/04/gulpfile.esm.js
+++ source/04/gulpfile.esm.js
@@ -4,7 +4,12 @@ const sass = require('gulp-sass')(require('sass'));
 
 function styles(done) {
     gulp.src('sass/**/*.scss')
-        .pipe(sass())
+        .pipe(sass({
+            errLogToConsole: false,
+            onError: function (err) {
+                return notify().write(err);
+            }
+        }))
         .pipe(gulp.dest('../public/css/'));
     done();
 }
